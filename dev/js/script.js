678038b9997aa3744e54e7f512fd57557604b6ea
Elena4550 <elenaboiko7580@gmail.com>
2025-08-31 17:42:30 +0300

    added features

--- dev/js/script.js
+++ dev/js/script.js
@@ -7,3 +7,18 @@ const toggleMenu =()=> {
 }
 
 burger.addEventListener('click', toggleMenu)
+
+
+// const burger = document.querySelector('.burger');
+// const navigation = document.querySelector('.first-screen__nav');
+// const card = document.querySelector('.motivation-card');
+
+// const toggleMenu = () => {
+//   burger.classList.toggle('open');
+//   navigation.classList.toggle('show');
+  
+//   // тут керуємо карткою
+//   card.classList.toggle('hidden', navigation.classList.contains('show'));
+// };
+
+// burger.addEventListener('click', toggleMenu);
